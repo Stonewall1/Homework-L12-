6f053d19169e2a89108e7dac7ebb352826993a43
Stonewall1 <impression97p@gmail.com>
2022-06-11 14:43:46 +0300

    Matrix Addition Task Improved

--- src/matrixAdditionTask/WritingMatrixes.java
+++ src/matrixAdditionTask/WritingMatrixes.java
@@ -5,28 +5,27 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WritingMatrixes {
-    public static void write(int[][] matrix1 , int[][] matrix2 , int[][] resultMatrix){
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter("src//matrixAdditionTask//MatrixAdditionResult.txt"))){
+    public static void write(int[][] matrix1, int[][] matrix2, int[][] resultMatrix) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src//matrixAdditionTask//MatrixAdditionResult.txt"))) {
             for (int i = 0; i < resultMatrix.length; i++) {
-                for (int j = 0; j < (resultMatrix[0].length*3 + 10); j++) {
-                    if(j < resultMatrix[0].length){
-                        writer.write(matrix1[i][j]+"\t");
-                    }
-                    else if((j >= 5 && j <= 9) || (j>=15 && j <= 19)){
-                        if(i == (resultMatrix.length / 2) && j == 7){
-                            writer.write("  +  ");
-                        }
-                        else if(i == (resultMatrix.length / 2) && j == 17){
-                            writer.write("  =  ");
-                        }
-                        else writer.write("     ");
-                    }
-                    else if(j> 9 && j <15){
-                        writer.write(matrix2[i][j - 10] + "\t");
-                    }
-                    else if(j > 19){
-                        writer.write(resultMatrix[i][j - 20] + "\t");
-                    }
+                for (int j = 0; j < matrix1[0].length; j++) {
+                    writer.write(matrix1[i][j] + "\t");
+                }
+
+                if (i == resultMatrix.length / 2) {
+                    writer.write("+\t");
+                } else writer.write("\t");
+
+                for (int j = 0; j < matrix2[0].length; j++) {
+                    writer.write(matrix2[i][j] + "\t");
+                }
+
+                if (i == resultMatrix.length / 2) {
+                    writer.write("=\t");
+                } else writer.write("\t");
+
+                for (int j = 0; j < resultMatrix[0].length; j++) {
+                    writer.write(resultMatrix[i][j] + "\t");
                 }
                 writer.write("\n");
             }
